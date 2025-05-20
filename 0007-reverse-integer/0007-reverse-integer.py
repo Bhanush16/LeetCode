e7882c9ec812073cec5d93861aeab47373ba64e2
Bhanush16 <bhanush1635@gmail.com>
2025-05-20 13:42:21 +0530

    Time: 21 ms (33.74%), Space: 12.5 MB (7.16%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def reverse(self, x):
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        rev = 0
+        while x != 0:
+            digit = x % 10
+            x = x // 10
+
+            # Check for overflow before adding digit
+            if rev > (INT_MAX - digit) // 10:
+                return 0
+
+            rev = rev * 10 + digit
+
+        return sign * rev
